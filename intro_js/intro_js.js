429f052e98a8e9865d08d880d8ecfa0469a5ee13
Robert Hutton <robert.hutton@unimelb.edu.au>
2016-04-20 14:06:19 +1000

    Complete part 1 of the javascript section

---
+++ intro_js/intro_js.js
@@ -0,0 +1,24 @@
+console.log("Hello, world!");
+
+// this is a comment
+
+//variables
+
+var my_variable="Hello Rob";
+var my_number=42;
+console.log(my_variable);
+console.log(my_number);
+
+function my_function (my_arg1, my_arg2) {
+  // my code
+  console.log(my_arg1+ my_arg2);
+}
+
+my_function("hello", "my buddies");
+
+// make a function that does some maths and console logs the output
+function multiply( arg1, arg2 ) {
+  console.log(arg1*arg2);
+}
+
+multiply(5, 10);
